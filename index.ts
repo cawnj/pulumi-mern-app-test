8dde457b8ff5368aba059483868ae2dee272f26b
Conor Joyce <conor.joyce27@mail.dcu.ie>
2023-08-02 11:56:04 +0100

    index: require more config

--- index.ts
+++ index.ts
@@ -6,6 +6,10 @@ const config = new pulumi.Config();
 const frontendPort = config.requireNumber("frontendPort");
 const backendPort = config.requireNumber("backendPort");
 const mongoPort = config.requireNumber("mongoPort");
+const mongoHost = config.require("mongoHost");
+const database = config.require("database");
+const nodeEnvironment = config.require("nodeEnvironment");
+const protocol = config.require("protocol")
 
 const stack = pulumi.getStack();
 
