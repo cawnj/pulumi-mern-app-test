215618e0b84d3b61f69bcd9418c1bbde9a358ad2
Conor Joyce <conor.joyce27@mail.dcu.ie>
2023-08-02 11:47:34 +0100

    index: pull images

--- index.ts
+++ index.ts
@@ -1,1 +1,21 @@
 import * as pulumi from "@pulumi/pulumi";
+import * as docker from "@pulumi/docker";
+
+const stack = pulumi.getStack();
+
+// Pull backend image
+const backendImageName = "backend";
+const backend = new docker.RemoteImage(`${backendImageName}Image`, {
+    name: "pulumi/tutorial-pulumi-fundamentals-backend:latest",
+});
+
+// Pull frontend image
+const frontendImageName = "frontend";
+const frontend = new docker.RemoteImage(`${frontendImageName}Image`, {
+    name: "pulumi/tutorial-pulumi-fundamentals-frontend:latest",
+});
+
+// Pull mongo image
+const mongoImage = new docker.RemoteImage("mongoImage", {
+    name: "pulumi/tutorial-pulumi-fundamentals-database-local:latest",
+});
